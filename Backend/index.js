6b0a9ed07df7e466a66bed134feaf4cc799cb61b
chaggysen <sen.wang@mail.mcgill.ca>
2021-02-01 20:24:19 -0500

    implement auth0

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
+
+const { auth } = require("express-openid-connect");
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(req.oidc.isAuthenticated() ? "Logged In" : "Logged out");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
