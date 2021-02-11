11fb53d07d9a822cb81c887b10f1773d03e7f93e
Onur Cayci <onur.cayci@outlook.com>
2021-02-11 14:44:24 -0500

    Move password hashing from register route to pre-save

--- Backend/Models/student.js
+++ Backend/Models/student.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const student = new mongoose.Schema({
   username: {
@@ -14,5 +15,14 @@ const student = new mongoose.Schema({
     default: [],
   },
 });
+
+student.pre('save', async function(next) {
+    
+  // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+})
 
 module.exports = Student = mongoose.model("Students", student);
