93a7b8ae82a3778a8adfc9c74fc2be36cacdf6ca
chaggysen <sen.wang@mail.mcgill.ca>
2021-02-14 15:49:14 -0500

    more error handling

--- Backend/Routes/StudentController.js
+++ Backend/Routes/StudentController.js
@@ -10,7 +10,13 @@ const verify = require("./VerifyToken");
 // ===== get a student by username =====
 // =====================================
 route.post("/getStudentByUsername", verify, function (req, res) {
+  if (!req.body.username){
+    return res.status(400).send("Please provide an username")
+  }
   Student.findOne({ username: req.body.username }, function (err, student) {
+    if (!student){
+      return res.status(400).send("Student does not exist")
+    }
     if (!err) {
       res.send(student);
     } else {
@@ -22,6 +28,9 @@ route.post("/getStudentByUsername", verify, function (req, res) {
 // ===== delete an existing student =====
 // ======================================
 route.post("/deleteStudentAccount", verify, async function (req, res) {
+  if (!req.body.username){
+    return res.status(400).send("Please provide an username")
+  }
   const studentName = req.body.username;
 
   // checking if student is already in the database
@@ -29,12 +38,11 @@ route.post("/deleteStudentAccount", verify, async function (req, res) {
   if (!studentExist) {
     return res.status(400).send("Student does not exist");
   }
-
   Student.deleteOne({ username: studentName }, function (err, student) {
     if (err) {
       res.send(err);
     } else {
-      res.send("student account has been deleted");
+      res.send("Student account has been deleted");
     }
   });
 });
@@ -47,11 +55,18 @@ route.post("/addTaskToStudent", verify, async function (req, res) {
     description: req.body.description,
   });
 
+  if (!req.body.username){
+    return res.status(400).send("Please provide an username")
+  }
+
   const studentName = req.body.username;
   // console.log("student Name is : "+ studentName);
 
   Student.findOne({ username: studentName }, function (err, student) {
     //console.log("found student name  :" + student);
+    if (!student){
+      return res.status(400).send("Student does not exist")
+    }
 
     if (!err) {
       const studentTasks = student.tasks;
@@ -88,15 +103,20 @@ route.post("/updateStudentTask", verify, async function (req, res) {
     title: req.body.title,
     description: req.body.description,
   });
-
+  if (!req.body.username){
+    return res.status(400).send("Please provide an username")
+  }
+  if (!req.body.taskId){
+    return res.status(400).send("Please provide an taskId")
+  }
   const studentName = req.body.username;
   const taskId = req.body.taskId;
-  // TODO fix
-  if (!taskId) {
-    return res.status(400).send("Student does not exist");
-  }
+  
   Student.findOne({ username: studentName }, function (err, student) {
     //console.log("found student name  :" + student);
+    if (!student){
+      return res.status(400).send("Student does not exists")
+    }
 
     if (!err) {
       const studentTasks = student.tasks;
@@ -109,6 +129,10 @@ route.post("/updateStudentTask", verify, async function (req, res) {
           taskToBeDeleted = task;
         }
       });
+
+      if (!taskToBeDeleted){
+        return res.status(400).send("Task does not exists")
+      }
       const index = studentTasks.indexOf(taskToBeDeleted);
       // console.log(index);
       studentTasks.splice(index, 1, newTask);
@@ -135,8 +159,14 @@ route.post("/updateStudentTask", verify, async function (req, res) {
 // ===== get tasks of existing student =====
 // =========================================
 route.get("/getStudentTasks", verify, function (req, res) {
+  if (!req.body.username){
+    return res.status(400).send("Please provide an username")
+  }
   const studentName = req.body.username;
   Student.findOne({ username: studentName }, function (err, student) {
+    if (!student){
+      return res.status(400).send("Student does not exists")
+    }
     console.log("found student name  :" + student);
     if (!err) {
       res.send(student.tasks);
