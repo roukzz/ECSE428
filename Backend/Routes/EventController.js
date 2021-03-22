20d85d76f73fb59e088422408b43d54f89cc6c78
Legrandj <legrandj09@gmail.com>
2021-03-22 01:47:13 -0400

    Added error handling and completed testing of create

--- Backend/Routes/EventController.js
+++ Backend/Routes/EventController.js
@@ -19,6 +19,36 @@ route.post("/createNewEvent", verify, async function (req, res) {
   if (!req.body.creatorID) {
     return res.status(400).send("Please provide a creator ID");
   }
+  let error; // If  there is an error, this value becomes equal to error and then doesnt send the response
+  await Event.find({ creatorID: newEvent.creatorID }, function (err, docs) {
+    //TIL this is an async method. Why is this an async method. That took too much time.
+    if (err) {
+      console.log(err);
+      assert.fail();
+    } else {
+      for (let i = 0; i < docs.length; i++) {
+        let element = docs[i];
+        if (element.title == newEvent.title) {
+          error = "error";
+        }
+        if (
+          (newEvent.startTime > element.startTime &&
+            newEvent.startTime < element.endTime) ||
+          (newEvent.endTime > element.startTime &&
+            newEvent.endTime < element.endTime) ||
+          (newEvent.startTime < element.startTime &&
+            newEvent.endTime > element.endTime)
+        ) {
+          error = "error";
+        }
+      }
+    }
+  });
+
+  if (error === "error") {
+    return res.status(400).send("Error");
+  }
+
   const savedEvent = await newEvent.save();
   res.send(savedEvent);
 });
--- Backend/Tests/createNewEvent.test.js
+++ Backend/Tests/createNewEvent.test.js
@@ -54,7 +54,7 @@ describe("Create New Event", () => {
         location: "Montreal",
       })
       .set("auth-token", authToken);
-    console.log(res.text);
+
     expect(res.statusCode).toEqual(200);
     Event.findOne({ location: "Montreal" }, function (err, docs) {
       if (err) {
@@ -64,7 +64,52 @@ describe("Create New Event", () => {
         expect(JSON.stringify(docs.creatorID)).toEqual(
           JSON.stringify(savedStudent._id)
         );
+        expect(docs.title).toEqual("New Event!!!");
+        expect(docs.description).toEqual("Gonna be fun!");
       }
     });
+  });
+  it("Should fail to create a new event because of no creatorID", async () => {
+    const res = await request(app)
+      .post("/api/event/createNewEvent")
+      .send({
+        title: "New Event!!!",
+        description: "Gonna be fun!",
+        startTime: new Date(Date.UTC(2021, 3, 1, 14, 30, 0)),
+        endTime: new Date(Date.UTC(2021, 5, 7, 20, 0, 0)),
+        location: "Montreal",
+      })
+      .set("auth-token", authToken);
+
+    expect(res.statusCode).toEqual(400);
+  });
+  it("Should fail to create a new event because of overlapping events by creator", async () => {
+    const res = await request(app)
+      .post("/api/event/createNewEvent")
+      .send({
+        creatorID: savedStudent._id,
+        title: "New Event!!!",
+        description: "Gonna be fun!",
+        startTime: new Date(Date.UTC(2021, 3, 1, 14, 30, 0)),
+        endTime: new Date(Date.UTC(2021, 5, 7, 20, 0, 0)),
+        location: "Montreal",
+      })
+      .set("auth-token", authToken);
+
+    const res2 = await request(app)
+      .post("/api/event/createNewEvent")
+      .send({
+        creatorID: savedStudent._id,
+        title: "New Event!!!",
+        description: "Gonna be fun!",
+        startTime: new Date(Date.UTC(2021, 2, 1, 14, 30, 0)),
+        endTime: new Date(Date.UTC(2021, 6, 7, 20, 0, 0)),
+        location: "Montreal",
+      })
+      .set("auth-token", authToken);
+
+    expect(res.statusCode).toEqual(200);
+    console.log(res2.text);
+    expect(res2.statusCode).toEqual(400);
   });
 });
