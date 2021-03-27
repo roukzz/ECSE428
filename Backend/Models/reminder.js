b6179d67a133d767ddfe750851b1181d5cd1467f
roukzz <arabfarouk@hotmail.com>
2021-03-27 12:47:25 -0400

    add priority to reminder/unit tests

--- Backend/Models/reminder.js
+++ Backend/Models/reminder.js
@@ -4,13 +4,19 @@ const reminder = new mongoose.Schema({
   reminderDate: {
     type: Date,
   },
-  
+
   description: {
     type: String,
   },
   title: {
     type: String,
   },
+  priority: {
+    type: String,
+    
+  }
 });
+
+
 
 module.exports = Reminder = mongoose.model("Reminders", reminder);
