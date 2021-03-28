732aeb6443af5dfa143185bb28341b3058ae7194
Onur Cayci <onur.cayci@outlook.com>
2021-03-28 17:44:12 -0400

    Implement Join Event gherkin tests

--- Backend/Routes/EventController.js
+++ Backend/Routes/EventController.js
@@ -66,6 +66,9 @@ route.post("/getAllEvents", verify, async function (req, res) {
   });
 });
 route.post("/joinEvent", verify, async function (req, res) {
+  if (req.body.eventID === null) {
+    return res.status(400).send();
+  }
   Event.find({ _id: req.body.eventID }, function (err, docs) {
     if (err) {
       console.log(err);
