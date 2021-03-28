2233d2217d3239337226f5ff48049449b7fc066a
chaggysen <sen.wang@mail.mcgill.ca>
2021-03-28 13:16:46 -0400

    reset password unit test

---
+++ Backend/Tests/resetPassword.test.js
@@ -0,0 +1,74 @@
+const request = require("supertest");
+
+const Student = require("../Models/student");
+const app = require("../server");
+const { connect, closeDatabase, clearDatabase } = require("../testdb");
+
+// setup mock database connection before each test case
+// create a student before each test case
+beforeEach(async (done) => {
+  await connect();
+  let student = Student({
+    username: "student",
+    password: "password",
+    email: "email@email.com",
+  });
+  await student.save();
+  done();
+});
+
+// drop the database after each test case
+// close the mock database connection after each test
+afterEach(async (done) => {
+  await clearDatabase();
+  done();
+});
+
+afterAll(async (done) => {
+  await closeDatabase();
+  done();
+});
+
+describe("User Reset Password Test", () => {
+  it("should fail due to username validation", async () => {
+    const res = await request(app)
+      .post("/api/authentication/forgotPassword")
+      .send({
+        username: "st",
+        email: "email@email.com",
+      });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("should fail due to empty email", async () => {
+    const res = await request(app)
+      .post("/api/authentication/forgotPassword")
+      .send({
+        username: "st",
+      });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("should fail due to empty username", async () => {
+    const res = await request(app)
+      .post("/api/authentication/forgotPassword")
+      .send({
+        email: "email@email.com",
+      });
+
+    expect(res.statusCode).toEqual(400);
+  });
+
+  it("should succeed receive reset link", async () => {
+    const res = await request(app)
+      .post("/api/authentication/forgotPassword")
+      .send({
+        username: "student",
+        email: "email@email.com",
+      });
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
